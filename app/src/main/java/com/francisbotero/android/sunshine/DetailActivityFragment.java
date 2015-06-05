32a4c7b9a5130867aea2c69e95bdbde6e3cbd175
Francis Botero <francis.botero@gmail.com>
2015-06-05 01:12:07 -0400

    Detail view shows forecast string

--- app/src/main/java/com/francisbotero/android/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/francisbotero/android/sunshine/DetailActivityFragment.java
@@ -1,15 +1,15 @@
 package com.francisbotero.android.sunshine;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class DetailActivityFragment extends Fragment {
 
     public DetailActivityFragment() {
@@ -18,6 +18,13 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        if (intent != null  && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
+            TextView forecastStringView = (TextView)rootView.findViewById(R.id.forecast_string);
+            forecastStringView.setText(forecastString);
+        }
+        return rootView;
     }
 }
